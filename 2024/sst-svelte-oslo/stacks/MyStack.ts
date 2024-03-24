8f66cc38438e12e4f101bdb7f59ef1d41b1d60ba
Michael Li <mealted.lemon@gmail.com>
2024-03-24 11:23:52 -0700

    dependencies working for api but not site
    Dependencies are in lambda but don't work for site

--- 2024/sst-svelte-oslo/stacks/MyStack.ts
+++ 2024/sst-svelte-oslo/stacks/MyStack.ts
@@ -3,11 +3,32 @@ import { StackContext, Api, SvelteKitSite } from "sst/constructs";
 export function API({ stack }: StackContext) {
   const site = new SvelteKitSite(stack, "site", {
     path: "packages/frontend/",
-    nodejs: { install: ["oslo"] },
+    // this still doesn't work even though files are in lambda
+    nodejs: {
+      install: [
+        "oslo",
+        "@node-rs/argon2",
+        "@node-rs/argon2-linux-x64-gnu",
+        "@node-rs/bcrypt",
+        "@node-rs/bcrypt-linux-x64-gnu",
+      ],
+    },
   });
   const api = new Api(stack, "api", {
-    // installs in lambda and resolves bundling issue but doesn't work
-    defaults: { function: { nodejs: { install: ["oslo"] } } },
+    // installs in lambda and works now
+    defaults: {
+      function: {
+        nodejs: {
+          install: [
+            "oslo",
+            "@node-rs/argon2",
+            "@node-rs/argon2-linux-x64-gnu",
+            "@node-rs/bcrypt",
+            "@node-rs/bcrypt-linux-x64-gnu",
+          ],
+        },
+      },
+    },
     routes: {
       "GET /": "packages/functions/src/lambda.handler",
     },
